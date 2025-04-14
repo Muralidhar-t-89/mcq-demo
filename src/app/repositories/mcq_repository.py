3749eb65ada52fc3c0bf2d169f9c40593df464fa
Muralidhar-t-89 <muralidhar.t93@gmail.com>
2025-04-14 15:41:13 +0530

    Add get_by_question_and_options method for duplicate MCQ check

--- src/app/repositories/mcq_repository.py
+++ src/app/repositories/mcq_repository.py
@@ -1,8 +1,8 @@
+from sqlalchemy import func
 from sqlalchemy.exc import SQLAlchemyError
 
 from src.app.repositories.base_repository import BaseRepository
 from src.app.entities.mcq import MCQ
-
 
 
 class MCQRepository(BaseRepository):
@@ -10,7 +10,10 @@ class MCQRepository(BaseRepository):
         super().__init__()
         self.session = session
 
-    def get_one(self, item_id: int) -> MCQ:
+    def get_one(self, item_id: int) -> MCQ | None:
+        """
+        Retrieve a single MCQ by its ID
+        """
         try:
             mcq = self.session.query(MCQ).filter(MCQ.id == item_id).first()
             return mcq
@@ -19,6 +22,9 @@ class MCQRepository(BaseRepository):
             return None
 
     def get_all(self) -> list[MCQ]:
+        """
+        Retrieve all MCQs
+        """
         try:
             mcqs = self.session.query(MCQ).all()
             return mcqs
@@ -26,10 +32,19 @@ class MCQRepository(BaseRepository):
             print(f"Error retrieving all MCQs: {e}")
             return []
 
+    def get_by_category(self, category_id: int) -> list[MCQ]:
+        """
+        Retrieve all MCQs by category ID
+        """
+        mcq_by_category = self.session.query(MCQ).filter(MCQ.category == category_id).all()
+        return mcq_by_category
+
     def add(self, item: MCQ) -> bool:
+        """
+        Add a new MCQ to the database
+        """
         try:
             self.session.add(item)
-            self.session.commit()
             return True
         except SQLAlchemyError as e:
             print(f"Error adding MCQ: {e}")
@@ -37,24 +52,22 @@ class MCQRepository(BaseRepository):
             return False
 
     def update(self, item: MCQ) -> bool:
+        """
+        Update an existing MCQ
+        """
         try:
-            existing_mcq = self.get_one(item.id)
-            if existing_mcq:
-                existing_mcq.question = item.question
-                existing_mcq.options = item.options
-                existing_mcq.correct_option = item.correct_option
-                existing_mcq.category = item.category
-                existing_mcq.created_by = item.created_by
-                existing_mcq.created_date = item.created_date
-                self.session.commit()
-                return True
-            return False
+            self.session.commit()
+            self.session.refresh(item)
+            return True
         except SQLAlchemyError as e:
-            print(f"Error updating MCQ: {e}")
+            print(f"Error adding MCQ: {e}")
             self.session.rollback()
             return False
 
     def delete(self, item_id: int) -> bool:
+        """
+        Delete an MCQ from the database
+        """
         try:
             mcq = self.get_one(item_id)
             if mcq:
@@ -66,3 +79,17 @@ class MCQRepository(BaseRepository):
             print(f"Error deleting MCQ with id {item_id}: {e}")
             self.session.rollback()
             return False
+
+    def get_by_question_and_options(self, question: str, options: list[str]) -> MCQ | None:
+        """
+        Retrieves a MCQ from the database that matches both the question text and the provided options.
+        """
+        mcqs = self.session.query(MCQ).filter(func.lower(func.trim(MCQ.question)) == question).all()
+        print("mcqs check:", mcqs)
+
+        for mcq in mcqs:
+            stored_options = sorted(opt.strip().lower() for opt in mcq.options)
+            print("stored_options check:", stored_options)
+            if stored_options == options:
+                return mcq
+        return None
