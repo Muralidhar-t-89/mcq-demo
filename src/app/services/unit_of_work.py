25b00c1b6eb1a9f5e96dabc9452bd36b623c2819
Muralidhar-t-89 <muralidhar.t93@gmail.com>
2025-04-17 20:20:45 +0530

    Move session initialization from __enter__ to __init__ in MCQUnitOfWork

--- src/app/services/unit_of_work.py
+++ src/app/services/unit_of_work.py
@@ -1,23 +1,11 @@
 import abc
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 from src.app.build_db import DEFAULT_SESSION_FACTORY
-from src.app.config import settings
 from src.app.repositories.category_repository import CategoryRepository
 from src.app.repositories.log_repo import LogRepository
 from src.app.repositories.mcq_repository import MCQRepository
 from src.app.repositories.quiz_attempt_repository import QuizAttemptRepository
 from src.app.repositories.user_repository import UserRepository
-
-# DATABASE_URL = (
-#     f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}@"
-#     f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
-# )
-#
-# DEFAULT_SESSION_FACTORY = sessionmaker(
-#     bind=create_engine(DATABASE_URL, echo=True)
-# )
 
 
 class BaseUnitOfWork(abc.ABC):
@@ -45,9 +33,9 @@ class BaseUnitOfWork(abc.ABC):
 class MCQUnitOfWork(BaseUnitOfWork):
     def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
         self.session_factory = session_factory
+        self.session = self.session_factory()
 
     def __enter__(self):
-        self.session = self.session_factory()
         self.user_repo = UserRepository(session=self.session)
         self.mcq_repo = MCQRepository(session=self.session)
         self.category_repo = CategoryRepository(session=self.session)
